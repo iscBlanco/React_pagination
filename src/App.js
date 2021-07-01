7c246064f09ce94e20aea1a3f4c4b3d2d4f66425
Jose Luis Blanco Aguirre <iscblanco@gmail.com>
2021-06-30 20:02:24 -0500

    React simple exaple about pagination with bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import Posts from './components/Posts'
+import Pagination from './components/Pagination'
 
-function App() {
+export default function App() {
+  const [posts, setPosts] = React.useState([])
+  const [loading, setLoading] = React.useState(false)
+  const [currentPage, setCurrentPage] = React.useState(1)
+  const [postPerPage] = React.useState(7)
+
+  React.useEffect(()=>{
+    const fetchPosts= async()=>{
+      setLoading(true);
+      const res= await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    }
+
+    fetchPosts();
+  },[])
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postPerPage;
+  const indexOfFirstPost = indexOfLastPost - postPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  //Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mt-5'>
+      <h1 className='text-primary mb-3'>My Blog</h1>
+      <Posts
+        posts={currentPosts}
+        loading={loading}
+      />
+      <Pagination
+        totalPosts={posts.length}
+        postsPerPage={postPerPage}
+        paginate={paginate}
+      />
+      <button className='btn btn-success'>click</button>
     </div>
-  );
+  )
 }
 
-export default App;
